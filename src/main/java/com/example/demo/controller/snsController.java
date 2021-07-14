841b8339d4016063c4c72d4727a150cc45483c39
KENKEN1998 <deimostran@gmail.com>
2021-07-14 12:45:31 +0900

    login

--- src/main/java/com/example/demo/controller/snsController.java
+++ src/main/java/com/example/demo/controller/snsController.java
@@ -1,54 +1,57 @@
 package com.example.demo.controller;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 import com.example.demo.model.Users;
+
+
 
 @Controller
 public class snsController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String homePage(HttpSession session, Model model) {
+
+		return "login";
+	}
+
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String index(@PathVariable("name") String name, @PathVariable("password") String password,
+	public String index(Users user,
 			HttpSession session, Model model) {
 
+		String sql = "SELECT * FROM users WHERE name = ? AND password = ?";
+		
+		List<Map<String, Object>> userList = jdbcTemplate.queryForList(sql, user.getName(), user.getPassword());
+		
+		model.addAttribute("name", user.getName());
+		model.addAttribute("password", user.getName());
+        return userList.size() > 0 ? "home" : "error";
 
-		String sql = "SELECT * FROM users WHERE name =" + name + " AND password = " + password;
-
-		Users user = jdbcTemplate.queryForObject(sql, new RowMapper<Users>() {
-			  @Override
-			    public Users mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-			        Users user = new Users();
-			        user.setId(rs.getInt("ID"));
-			        user.setName(rs.getString("NAME"));
-			        user.setPassword(rs.getString("AGE"));
-			        user.setImgpath(rs.getString("IMGPATH"));
-			        return user;
-
-			    }
-		});
-
-		model.addAttribute("name", name);
-		model.addAttribute("password", password);
-
-	return user != null ? "home" : "error";
+	}
+	
+	@RequestMapping(path = "/login/new", method = RequestMethod.GET)
+	public String newUser(Model model) {
+		
+		Users user = new Users();
+		
+		model.addAttribute("user", user);
+		model.addAttribute("pageTitle", "Create New User");
+		
+		return "users/user_form";
 	}
 
 }
