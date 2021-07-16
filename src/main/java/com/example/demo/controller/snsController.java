b42c9792671e869122033c674858517308657b65
KENKEN1998 <deimostran@gmail.com>
2021-07-16 13:36:26 +0900

    add save

--- src/main/java/com/example/demo/controller/snsController.java
+++ src/main/java/com/example/demo/controller/snsController.java
@@ -10,9 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 import com.example.demo.model.Users;
 
@@ -35,11 +35,24 @@ public class snsController {
 
 		String sql = "SELECT * FROM users WHERE name = ? AND password = ?";
 		
-		List<Map<String, Object>> userList = jdbcTemplate.queryForList(sql, user.getName(), user.getPassword());
+		List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, user.getName(), user.getPassword());
 		
-		model.addAttribute("name", user.getName());
-		model.addAttribute("password", user.getName());
-        return userList.size() > 0 ? "home" : "error";
+//		model.addAttribute("name", user.getName());
+//		model.addAttribute("password", user.getName());
+		
+//        return userList.size() > 0 ? "home" : "error";
+		System.out.println(list.size());
+		
+		if (list.size() == 1) {
+			Map<String,Object> u = list.get(0);
+            Users principal = new Users();
+            principal.setId((Integer) u.get("id"));
+            principal.setName((String) u.get("name"));
+            model.addAttribute("principal",principal);
+			return "home";
+		}else {
+			return "redirect:/login";
+		}
 	}
 	
 	@RequestMapping(path = "/login/new", method = RequestMethod.GET)
@@ -47,8 +60,9 @@ public class snsController {
 		return "users/user_form";
 	}
 	
-	@RequestMapping(path = "/login/save", method = RequestMethod.POST)
-	public String saveNewUser(Users user, HttpSession session, Model model) {
+	@RequestMapping(path = "/save", method = RequestMethod.POST)
+	public String saveNewUser(Users user, Model model) {
+		
 		
 		String sql = "INSERT INTO users (name, password)"
                 + " VALUES (?, ?)";
@@ -58,23 +72,22 @@ public class snsController {
 		return "login";
 	}
 	
-	@RequestMapping(path = "/home/edit", method = RequestMethod.GET)
-	public String editUser(Users user, Model model) {
+	@RequestMapping(path = "/home/edit/{id}", method = RequestMethod.GET)
+	public String editUser(@PathVariable("id") Integer id, Model model) {
 		
-		
-		model.addAttribute("user", user);
-		return "users/editUser_form";
+		String sql = "SELECT * FROM users WHERE id = ?";
+		List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, id);
+		if (list.size() == 1) {
+			Map<String,Object> u = list.get(0);
+            Users user = new Users();
+            user.setId((Integer) u.get("id"));
+            user.setName((String) u.get("name"));
+            model.addAttribute("user",user);
+			return "users/editUser_form";
+		}else {
+			return "redirect:/login";
+		}
 	}
 	
-	@RequestMapping(path = "/edit/save", method = RequestMethod.POST)
-	public String saveEditUser(Users user, HttpSession session, Model model) {
-		
-		String sql =  "UPDATE users SET name=?, password=? "
-                + "WHERE id=?";
-        
-		jdbcTemplate.update(sql, user.getName(), user.getPassword(), user.getId());
-
-		return "home";
-	}
 
 }
